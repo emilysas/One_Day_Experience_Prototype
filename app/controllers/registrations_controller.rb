6c374bd9c0e879433b0cf11b7e7a7e829a68c684
Sanda Golcea <sanda.golcea@gmail.com>
2015-02-24 23:15:14 +0000

    Redirect from professional sign up to profile done

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,11 @@
+class RegistrationsController < Devise::RegistrationsController
+  protected
+
+  def after_sign_up_path_for(resource)
+    '/profiles/new'
+  end
+
+  def after_inactive_sign_up_path_for(resource)
+    '/profiles/new'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :students, :controllers => {:omniauth_callbacks => "students/omniauth_callbacks"}
-  devise_for :professionals
+  devise_for :professionals, :controllers => { :registrations => "registrations" }
   resources :professions
   resources :profiles
   root to: 'application#index'
-
   # devise_scope :student do
     # get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_student_session
   # end
