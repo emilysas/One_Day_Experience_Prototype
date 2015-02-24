bc590bf4d2d40a74eafece51df48adaa0ecaf0b5
Emily Sas <emily.bronwen.sas@gmail.com>
2015-02-24 18:28:19 +0000

    adding new profile form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :students, :controllers => {:omniauth_callbacks => "students/omniauth_callbacks"}
   devise_for :professionals
   resources :professions
+  resources :profiles
   root to: 'application#index'
 
   # devise_scope :student do
