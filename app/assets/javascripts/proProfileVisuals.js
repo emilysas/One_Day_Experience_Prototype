d0fd3ad1a5fa66aebbd6cff631df8dd094f27456
jjlakin <j.lakin@gmail.com>
2015-02-26 15:28:07 +0000

    fixed half the bug of the side bar and begun the profiles page

---
+++ app/assets/javascripts/proProfileVisuals.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	if ($("#darkPage").length){
+    $(document).ready(function(){
+    	console.log('ready');
+    	$('#sidebar').css('background-color', '#303030');
+    	$('.links').css('color', '#E4E4E4');
+			
+			
+
+    });
+	}
+});
