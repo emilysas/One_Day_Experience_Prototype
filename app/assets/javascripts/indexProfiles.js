22c1980bc2ae15e7e5fafc1f633017938a380711
Sanda Golcea <sanda.golcea@gmail.com>
2015-02-26 01:32:41 +0000

    Next button now functional on the index page

--- app/assets/javascripts/indexProfiles.js
+++ app/assets/javascripts/indexProfiles.js
@@ -1,15 +1,36 @@
 $(document).ready(function(){
-
-  $.get( "http://localhost:3000//api/profiles.json?page=1", function( data ) {
-    // TODO: display this info properly
-    $( ".result" )
-    .append( "<b>Name:</b> " + data[1].name )
-    .append( "<br><b>Company:</b> " + data[1].company )
-    .append( "<br><b>Info:</b> " + data[1].info );
+  // TODO: refactor this like crazy loco - mainly not doing a get for every single obj
+  var pageNumber = 1;
+  var objectNumber = 0;
+  getProfileInfo(pageNumber, objectNumber);
+  // yes is the id of the right circle
+  // TODO: change all id's to match purpose
+  $("#yes").on('click', function(){
     
-    var src = $("#tokyo").attr("src").replace("/images/tokyo-city.jpg", data[1].image_url);
-    $("#tokyo").attr("src", src);
+    // get to next elem, or first elem on next page
+    objectNumber++;
+    if (objectNumber>=3) {
+      objectNumber=1;
+      pageNumber++;
+    }
+    getProfileInfo(pageNumber, objectNumber);
+  });
+
+})
+
+var getProfileInfo = function (pageNumber, objectNumber) {
+
+  $.get( "http://localhost:3000//api/profiles.json?page="+pageNumber, function( data ) {
+    
+    // display basic info
+    $('#buddy-name').html(data[objectNumber].name);
+    $('#buddy-company').html(data[objectNumber].company);
+    $('#buddy-info').html(data[objectNumber].info);
+    console.log("Page = " + pageNumber + " Object = " + objectNumber);
+
+    // display image
+    $("#tokyo").attr("src", data[objectNumber].image_url);
     
   }, "json");
-    
-})
+  
+}
