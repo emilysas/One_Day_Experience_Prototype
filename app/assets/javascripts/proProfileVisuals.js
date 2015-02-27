bde28889f066d8678698b587a4e95cd28a7b8813
Sanda Golcea <sanda.golcea@gmail.com>
2015-02-27 01:40:39 +0000

    Fixes: profile pic on show page, minor cleans and comments

--- app/assets/javascripts/proProfileVisuals.js
+++ app/assets/javascripts/proProfileVisuals.js
@@ -4,9 +4,6 @@ $(document).ready(function(){
     	console.log('ready');
     	$('#sidebar').css('background-color', '#303030');
     	$('.links').css('color', '#E4E4E4');
-			
-			
-
     });
 	}
 });
