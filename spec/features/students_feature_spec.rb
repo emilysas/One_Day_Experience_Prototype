cb8bc1f2332771f764253de2a95570bee769fe9f
Emily Sas <emily.bronwen.sas@gmail.com>
2015-03-01 13:38:11 +0000

    passing tests - when signed in can see 'sign out' link and cannot see 'sign in' link

--- spec/features/students_feature_spec.rb
+++ spec/features/students_feature_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 context "a user not signed in and on the homepage" do
+
   it "should see a 'Sign in' link" do
     visit('/')
     expect(page).to have_link('Sign in')
@@ -9,6 +10,25 @@ context "a user not signed in and on the homepage" do
   it "should not see 'Sign out' link" do
     visit('/')
     expect(page).not_to have_link('Sign out')
+  end
+
+end
+
+context "a user signed in and on the homepage" do
+  
+  before do
+    student = create(:student)
+    login_as(student, :scope => :student)
+  end
+  
+  it "should see a 'Sign out' link" do
+    visit('/')
+    expect(page).to have_link('Sign out')
+  end
+
+  it "should not see 'Sign in' link" do
+    visit('/')
+    expect(page).not_to have_link('Sign in')
   end
 
 end
