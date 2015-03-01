aea0c93ee46ac730fd52f1eee26407b188c3dc17
Emily Sas <emily.bronwen.sas@gmail.com>
2015-03-01 14:41:47 +0000

    passing tests - can sign up and sign in as a professional

--- spec/features/signin_feature_spec.rb
+++ spec/features/signin_feature_spec.rb
@@ -42,6 +42,28 @@ feature 'Sign in' do
       login_as(student, :scope => :student)     
       expect(Student.last.name).to eq "Jonny"
       expect(current_path).to eq('/')
+      logout(:student)
+    end
+
+    it "should allow a user to sign up as a professional" do
+      visit '/'
+      click_link 'Sign in'
+      click_link 'Sign Up As A Professional'
+      fill_in "Email", with: "professional_test@example.com"
+      fill_in "Password", with: "12341234"
+      fill_in "Password confirmation", with: "12341234"
+      click_button 'Sign up'
+      expect(Professional.last.email).to eq "professional_test@example.com"
+      expect(current_path).to eq new_profile_path
+    end
+
+    it "should allow a user to sign in as a student" do
+      visit '/'
+      professional = create(:professional)
+      login_as(professional, :scope => :professional)     
+      expect(Professional.last.email).to eq "professional@example.com"
+      expect(current_path).to eq('/')
+      logout(:professional)
     end
 
   end
