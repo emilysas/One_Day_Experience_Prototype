e965181529bb1d6ca41558b86914df8eb4d74930
Emily Sas <emily.bronwen.sas@gmail.com>
2015-03-01 13:36:20 +0000

    passing tests - a user who is not signed in can see a 'sign in' link and cannot see a 'sign out' link

--- spec/features/mailer_feature_spec.rb
+++ spec/features/mailer_feature_spec.rb
@@ -1,6 +1,4 @@
 require 'rails_helper'
-require './app/models/professional.rb'
-require './app/models/profile.rb'
 
 describe 'StudentMailer' do 
   
--- spec/features/students_feature_spec.rb
+++ spec/features/students_feature_spec.rb
@@ -1,28 +1,15 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# context "student not signed in and on the homepage" do
-#   it "should see a 'Student sign in' link and a 'Student sign up' link" do
-#     visit('/')
-#     expect(page).to have_link('Student sign in')
-#     expect(page).to have_link('Student sign up')
-#   end
+context "a user not signed in and on the homepage" do
+  it "should see a 'Sign in' link" do
+    visit('/')
+    expect(page).to have_link('Sign in')
+  end
 
-#   it "should not see 'Student sign out' link" do
-#     visit('/')
-#     expect(page).not_to have_link('Sign out')
-#   end
+  it "should not see 'Sign out' link" do
+    visit('/')
+    expect(page).not_to have_link('Sign out')
+  end
 
-#   it "should see "
-#   # it "should see a 'Student sign in' link and a 'Student sign up' link" do
-#   #   visit('/')
-#   #   expect(page).to have_link('Student sign in')
-#   #   expect(page).to have_link('Student sign up')
-#   # end
-
-#   # it "should not see 'Student sign out' link" do
-#   #   visit('/')
-#   #   expect(page).not_to have_link('Sign out')
-#   # end
-
-# end
+end
 
