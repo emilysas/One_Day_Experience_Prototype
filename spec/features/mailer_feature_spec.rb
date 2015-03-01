c9c1a5836dc3e6190c998f213df35cc71b1df032
Emily Sas <emily.bronwen.sas@gmail.com>
2015-03-01 13:10:33 +0000

    passing test - a user cannot send an email unless logged in

--- spec/features/mailer_feature_spec.rb
+++ spec/features/mailer_feature_spec.rb
@@ -12,11 +12,25 @@ describe 'StudentMailer' do
       login_as(student, :scope => :student)
     end
 
-    it "should be able to access an email form on a profile page" do
+    it "should be able to send an email on a profile page" do
       visit('/profiles/1')
       click_link 'Register Interest'
       expect(page).to have_content('Your email has been sent')
       expect(current_path).to eq('/profiles/1')
+    end
+  end
+
+  context "a student has not logged in" do
+
+    before do
+      profile = create(:profile)
+      student = create(:student)
+    end
+
+    it "should not be able to send an email if not logged in as a student" do
+      visit('/profiles/1')
+      expect(page).not_to have_content('Register Interest')
+      expect(page).to have_content('Please sign in to contact Emily')
     end
 
   end
