28e8bc59cf8fd80f00ecb1584d61ff1702b972fe
Emily Sas <emily.bronwen.sas@gmail.com>
2015-03-01 17:11:53 +0000

    displaying error messages on profile form

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -7,8 +7,8 @@ class Profile < ActiveRecord::Base
   belongs_to :professional
   belongs_to :profession
 
-  validates_presence_of :name, message: "Please provide your name"
-  validates_presence_of :profession_id, message: "Please select your profession"
+  validates_presence_of :name, message: "- Please provide your name"
+  validates_presence_of :profession_id, message: "- Please select your profession"
 
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
