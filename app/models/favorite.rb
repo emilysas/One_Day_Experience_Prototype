72e9bd69fbfd0370004ce356acb36eb2fac10414
Emily Sas <emily.bronwen.sas@gmail.com>
2015-03-02 12:09:32 +0000

    profiles can only be appear in a student's favorites once

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,9 @@
 class Favorite < ActiveRecord::Base
   belongs_to :student
   belongs_to :favorited, polymorphic: true
+
+  validates :student_id, uniqueness: { 
+    scope: [:favorited_id],
+    message: 'can only favorite an item once'
+  }
 end
