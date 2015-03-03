17c8384a8ea21ab21a4897d81d044c1fff461ae1
Emily Sas <emily.bronwen.sas@gmail.com>
2015-03-03 10:40:45 +0000

    adding devise model and routes for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
   devise_for :students, :controllers => {:omniauth_callbacks => "students/omniauth_callbacks"}
   devise_for :professionals, :controllers => { :registrations => "registrations" }
 
@@ -16,18 +17,28 @@ Rails.application.routes.draw do
     get :send_email, to: 'profiles#send_email', as: :send_email
   end
 
-  # Student can add favorite profiles feature
+  authenticated :admin do
+    scope module: :admin do
+      resources :profiles, only: [] do
+        collection do
+          get :unverified, to: 'profiles#unverified'
+        end
+        member do
+          post :verify, to: 'profiles#verify'
+        end
+      end
+    end
+  end
+
+
   resources :favorite_profiles, only: [:create, :destroy]
-  get 'favorites', to: 'favorites#show'
+  get :favorites, to: 'favorites#show'
 
   get :sign_up_gate, to: 'application#sign_up_gate', as: :sign_up_gate
   get :sign_in_gate, to: 'application#sign_in_gate', as: :sign_in_gate
 
   get 'search', to: 'search#search'
 
-  get :verification, to: 'profiles#verification', as: :verification
-
-  post '/verify' => 'profiles#verify'
   # TODO: what is this for? still needed? 
   # devise_scope :student do
     # get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_student_session
