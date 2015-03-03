4c4a62c37d1c7ce1b1bdffbe2d379c914be8cd80
Emily Sas <emily.bronwen.sas@gmail.com>
2015-03-03 16:03:26 +0000

    refactoring create method in professionals controller

--- app/controllers/professionals_controller.rb
+++ app/controllers/professionals_controller.rb
@@ -2,16 +2,8 @@ class ProfessionalsController < Devise::RegistrationsController
 
   def create
     build_resource(registration_params)
-
     if resource.save
-      if resource.active_for_authentication?
-        set_flash_message :notice, :signed_up if is_navigational_format?
-        sign_up(resource_name, resource)
-        respond_with resource, :location => after_sign_up_path_for(resource)
-      else
-        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
-        respond_with resource, :location => after_sign_up_path_for(resource)
-      end
+      sign_up_status(resource)
     else
       clean_up_passwords
       respond_with resource
@@ -22,6 +14,17 @@ class ProfessionalsController < Devise::RegistrationsController
 
   def registration_params
     params.require(:user).permit(:email, :name, :password, :password_confirmation)
+  end
+
+  def sign_up_status(resource)
+    if resource.active_for_authentication?
+      set_flash_message :notice, :signed_up if is_navigational_format?
+      sign_up(resource_name, resource)
+      respond_with resource, :location => after_sign_up_path_for(resource)
+    else
+      set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+      respond_with resource, :location => after_sign_up_path_for(resource)
+    end
   end
 
 end
