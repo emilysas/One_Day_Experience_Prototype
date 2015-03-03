7659bbffe73b434fb3fb3de216d1a6b94adf0531
Sanda Golcea <sanda.golcea@gmail.com>
2015-03-03 15:44:45 +0000

    Favorites tests done

--- spec/features/favorite_feature_spec.rb
+++ spec/features/favorite_feature_spec.rb
@@ -7,28 +7,40 @@ feature 'Favorites' do
   context 'a signed in student' do
     before do
       login_as(student, :scope => :student)
+      visit profile_path(profile)
+      click_link('Add to favorites')
     end
 
     it 'should be able to favorite a professional' do
-      visit profile_path(profile)
-      click_link('Add to favorites')
       expect(page).to have_content('Remove from favorites')
     end
 
-    xit 'should be able to remove a professional from favorites' do
+    it 'should be able to remove a professional from favorites' do
+      click_link('Remove from favorites')
+      expect(page).to have_content('Add to favorites')
     end
-    xit 'should be able to see all his/her favorites' do
+    
+    it 'should be able to see all his/her favorites' do
+      visit favorites_path
+      expect(page).to have_content('Emily')
     end
+
+    it 'should not longer see a person that has been removed from favorites' do
+      click_link('Remove from favorites')
+      visit favorites_path
+      expect(page).not_to have_content('Emily')
+    end
+    
   end
 
   context 'without being a signed in' do
    
-   xit "cannot favorite a profile" do
-     expect{visit favorite_profiles_path}.to raise_error( ActionController::RoutingError)
+   it "cannot favorite a profile" do
+     expect{visit favorite_profile_path(profile)}.to raise_error( ActionController::RoutingError)
    end
 
-   xit "cannot see favorite profiles" do
-     expect{visit favorites_student_path}.to raise_error( ActionController::RoutingError)
+   it "cannot see favorite profiles" do
+     expect{visit favorites_path}.to raise_error( ActionController::RoutingError)
    end
 
  end
