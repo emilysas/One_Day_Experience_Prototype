0989776e8481ced7e91829ea6b841a3ffed031dc
jjlakin <j.lakin@gmail.com>
2015-03-05 20:37:26 +0000

    fixed jquery maps by changing the damn handles

--- app/assets/javascripts/proProfileVisuals.js
+++ app/assets/javascripts/proProfileVisuals.js
@@ -1,32 +1,30 @@
 	
 
 $(document).ready(function(){
-	var sectorImageWidth = $('#profile-image--sector').css('width')
+
+	var sectorImageWidth = $('#profile-image--sector').css('width');
 	
 	$('#profile-image--sector').css('height',sectorImageWidth);
 	$('#profile-text--individual').css('height',sectorImageWidth);
-	$('.profiles_map').css('height',sectorImageWidth);
+	$('.jsHandle').css('height',sectorImageWidth);
 	buildMap;
-	$('.profiles_map').css('margin-top','-100%');
+	$('.jsHandle').css('margin-top','-100%');
 
 	if ($("#darkPage").length){
-    $(document).ready(function(){
+    
     	console.log('ready');
     	$('#sidebar').css('background-color', '#303030');
     	$('.links').css('color', '#E4E4E4');
-    });
-	}
-	
+    
+	}	
 
-var startMargin = $('.profiles_map').css('margin-top')
-$('#jsLocationButton').hover(function(){		
-		
-		console.log(startMargin)
-		$('.profiles_map').css({marginTop:startMargin}).animate({marginTop: 0},1000)
+	var startMargin = $('.jsHandle').css('margin-top');
+	$('#jsLocationButton').hover(function(){				
+		$('.jsHandle').css({marginTop:startMargin}).animate({marginTop: 0},1000)
 	}, function() {	
-		$('.profiles_map').css({marginTop: 0}).animate({marginTop: startMargin},1000)
+		$('.jsHandle').css({marginTop: 0}).animate({marginTop: startMargin},1000)
 	});
 
-	
-
 });
+
+
